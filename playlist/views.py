eb64cde5c37c3e9d2305ef4f0a6177378f24c7a6
David Levine <davidaustinlevine@gmail.com>
2020-12-31 15:56:55 -0600

    edit views

--- playlist/views.py
+++ playlist/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from playlist.models import Artist, Album, Track
 
-# Create your views here.
+
+class StorefrontView(TemplateView):
+    template_name = 'storefront.html'
+
+    def get_context_data(self):
+        return {
+            'albums': Album.objects.all()
+        }
+
+
+def artist_view(request):
+    return render(request, 'artist_view.html', {
+        'artists': Artist.objects.all()
+    })
+
+def artist_detail_view(request, pk):
+    return render(request, 'artist_detail_view.html', {
+        'artist': Artist.objects.get(pk=pk)
+    })
